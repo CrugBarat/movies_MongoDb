96bbe215e4af1f27c094cd1658f5bde093ac4d59
Craig Barratt <craigtbarratt@gmail.com>
2020-05-02 15:34:05 +0100

    sets up server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const createRouter = require('./helpers/create_router.js');
+const MongoClient = require('mongodb').MongoClient
 
 const app = express();
-app.use(bodyParser.json());
 app.use(cors());
+
+MongoClient.connect('mongodb://localhost:27017')
+.then((client) => {
+  const db = client.db('movies_list');
+  const movieCollection = db.collection('movies');
+  const moviesRouter = createRouter(movieCollection);
+  app.use('/api/movies/', moviesRouter)
+})
+.catch(console.error);
+
+app.listen(3000, function () {
+  console.log(`Listening on port ${this.address().port}`);
+});
