e9a5d8de89d20fdd918fcd5723d95168a7dce4ba
Craig Barratt <craigtbarratt@gmail.com>
2020-05-02 18:36:48 +0100

    sets up routes

---
+++ server/helpers/create_router.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const ObjectID = equire('mongodb').ObjectID;
+
+const createRouter = (collection) => {
+
+  const router = express.Router();
+
+  router.get('', (req, res) => {
+    collection.find().toArray()
+    .then(docs => res.json(docs))
+    .catch((error) => {
+      console.error(error);
+      res.status(500);
+      res.json({error: error});
+    });
+  });
+
+  router.get(':id', (req, res) => {
+    const id = req.params.id;
+    collection.findOne({_id: ObjectID(id)})
+    .then(doc => res.json(doc))
+  });
+
+  router.delete(':id', (req, res) => {
+    const id = req.params.id;
+    collection.deleteOne({_id: ObjectID(id)})
+    .then(result => res.json(result))
+  });
+  
+}
